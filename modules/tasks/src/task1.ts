c87f7401548a6e2a76871ebe5612d9bb3023c6e0
Roman Suslikov <ua.romansyslikov@gmail.com>
2021-12-17 03:39:25 +0300

    Update task1.ts

--- modules/tasks/src/task1.ts
+++ modules/tasks/src/task1.ts
@@ -3,7 +3,7 @@ import { TODO } from '@flx-learn-ts/utils'
 /// Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
 /// Например, расстояние между (3, 0) и (0, 4) равно 5.
 export function trackLength(x1: number, y1: number, x2: number, y2: number): number {
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+    throw TODO
 }
 
 /// Пользователь задает целое число, большее 100 (например, 3801).
