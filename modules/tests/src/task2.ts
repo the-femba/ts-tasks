d3d480b85a6ec0c92ccc87a756ac33c826a2b031
Roman Suslikov <ua.romansyslikov@gmail.com>
2021-12-17 21:59:49 +0300

    Fixes

--- modules/tests/src/task2.ts
+++ modules/tests/src/task2.ts
@@ -12,7 +12,6 @@ describe('task2', () => {
         assert.strictEqual(17, minDivisor(8653))
         assert.strictEqual(2124679, minDivisor(2124679))
         assert.strictEqual(1073676287, minDivisor(1073676287))
-        assert.strictEqual(Number.MAX_VALUE, minDivisor(Number.MAX_VALUE))
     })
 
     it("fib()", () => {
